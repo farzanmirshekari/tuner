56a4370f22ca46ec175e4b8922d293784f58d7cf
Farzan Mirshekari <farzanmirshekari@gmail.com>
2024-08-22 15:45:10 -0400

    SPSC queue for audio stream

---
+++ ThreadSafeQueue.py
@@ -0,0 +1,24 @@
+from threading import Lock
+
+class ThreadSafeQueue(object):
+    def __init__(self):
+        self.queue = []
+        self.max_size = 10
+        self.lock = Lock()
+
+    def push(self, item):
+        with self.lock:
+            self.queue.append(item)
+            if len(self.queue) > self.max_size:
+                self.queue.pop(0)
+            
+    def pop(self):
+        with self.lock:
+            if len(self.queue) > 0:
+                return self.queue.pop(0)
+            else:
+                return None
+            
+    def size(self):
+        with self.lock:
+            return len(self.queue)
