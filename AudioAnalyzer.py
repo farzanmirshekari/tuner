c049027f99094cda06ba7f9d8032cd14b76b8d34
Farzan Mirshekari <farzanmirshekari@gmail.com>
2024-08-24 20:23:18 -0400

    Can now identify dominant frequency of 440Hz A as 440Hz

--- AudioAnalyzer.py
+++ AudioAnalyzer.py
@@ -1,19 +1,25 @@
-from threading import Thread
-
 from ThreadSafeQueue import ThreadSafeQueue
 
-class AudioAnalyzer(Thread):
+import time
+from threading import Thread
 
+class AudioAnalyzer(Thread):
     def __init__(self, queue: ThreadSafeQueue):
         Thread.__init__(self)
         self.queue = queue
+        self.running = False
 
     def run(self):
-        while True:
-            if not self.queue.size() == 0:
-                frequency = self.queue.pop()
-                print("Dominant Frequency: ", frequency, "Hz")
+        self.running = True
+
+        while self.running:
+            frequency = self.queue.pop()
+            if frequency is not None:
+                print("Dominant frequency:", frequency, "Hz")
+            else:
+                time.sleep(0.02)
 
     def stop(self):
-        self.queue.queue.clear()
-        self.queue.queue.append(None)
+        self.running = False
+        self.queue.clear()
+        self.queue.push(None)
