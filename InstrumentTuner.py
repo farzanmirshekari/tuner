322d8ca5f0eaa08f07fe1418c2905affa0b2f41c
Farzan Mirshekari <farzanmirshekari@gmail.com>
2024-09-03 17:40:21 -0400

    Add note_to_frequency function

--- InstrumentTuner.py
+++ InstrumentTuner.py
@@ -7,7 +7,11 @@ import time
 
 class InstrumentTuner(QThread):
 
-    note_detected = pyqtSignal(float, float, str)
+    frequency_detected = pyqtSignal(float)
+
+    NOTE_NAMES = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+    A4 = 440
+    C0 = A4 * 2 ** (-4.75)
 
     def __init__(self, queue: ThreadSafeQueue):
         QThread.__init__(self)
@@ -16,21 +20,35 @@ class InstrumentTuner(QThread):
 
     @staticmethod
     def frequency_to_note(frequency):
-        A4 = 440
-        C0 = A4 * 2 ** (-4.75)
-        name = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
-        h = round(12 * np.log2(frequency / C0))
+        h = round(12 * np.log2(frequency / InstrumentTuner.C0))
         octave = h // 12
         n = h % 12
-        return name[n] + str(octave)
+        return f"{InstrumentTuner.NOTE_NAMES[n]}{octave}"
     
+    @staticmethod
+    def note_to_frequency(note: str):
+        if len(note) == 0:
+            return -1
+        
+        if note[-1].isdigit():
+            note_name = note[:-1]
+            octave = int(note[-1])
+        else:
+            note_name = note
+            octave = 4
+
+        if note_name not in InstrumentTuner.NOTE_NAMES:
+            return -1
+        n = InstrumentTuner.NOTE_NAMES.index(note_name)
+        return InstrumentTuner.C0 * 2 ** ((n + octave * 12) / 12)
+
     def run(self):
         self.running = True
 
         while self.running:
             frequency = self.queue.pop()
             if frequency is not None and frequency > 0:
-                self.frequency_to_note(frequency)
+                self.frequency_detected.emit(frequency)
             else:
                 time.sleep(0.5)
 
